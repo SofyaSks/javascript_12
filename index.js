bcc04bcb71675ad782d3050ac62ec778e9eb84b8
Sofya Surovneva <ssurovneva@mail.ru>
2025-02-18 22:04:21 +0300

    подсчёт возраста

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ function colorize(){
         anq.style.background = 'lightblue'
     }
 }
+
+//
+
+let date = document.querySelector('[type = date')
+let nowDate = new Date()
+let nowYear = nowDate.getFullYear()
+
+
+function countAge(){
+    console.log(nowYear - date.valueAsDate.getFullYear());
+    age.value = nowYear - date.valueAsDate.getFullYear()
+
+}
